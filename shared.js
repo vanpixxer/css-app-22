1b1ac70a082f11e5c6af53881cd30c66d32d5441
Darrell Chaddock <webtastic.dc@gmail.com>
2022-08-10 15:52:21 -0700

    Added additional styles with CSS Flexbox

--- shared.js
+++ shared.js
@@ -10,36 +10,44 @@ var mobileNav = document.querySelector(".mobile-nav");
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
-    // modal.style.display = "block";
+     //modal.style.display = "block";
     // backdrop.style.display = "block";
-    // modal.className = 'open'; // This will actually overwrite the complete class list
-    modal.classList.add("open");
-    backdrop.classList.add("open");
+     //modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
 backdrop.addEventListener("click", function() {
-  // mobileNav.style.display = 'none';
-  mobileNav.classList.remove("open");
+  //mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
   closeModal();
+ // mobileNav.classList.remove("open");
+ // closeModal();
 });
+
 
 if (modalNoButton) {
   modalNoButton.addEventListener("click", closeModal);
 }
 
 function closeModal() {
-  //   backdrop.style.display = "none";
-  //   modal.style.display = "none";
-  if (modal) {
-    modal.classList.remove("open");
+    //backdrop.style.display = "none";
+    // modal.style.display = "none";
+    if (modal) {
+       modal.classList.remove('open');
+       backdrop.classList.remove('open');
+    }
+    
+ // if (modal) {
+  //  modal.classList.remove("open");
   }
-  backdrop.classList.remove("open");
-}
+  //backdrop.classList.remove("open");
+//}
 
 toggleButton.addEventListener("click", function() {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
-  mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 });
